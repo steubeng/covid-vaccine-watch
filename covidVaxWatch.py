24d4ef955a54bf9a85975dd7fc2fc5a4f07f21c0
steubeng <steubeng@gmail.com>
2021-03-02 14:36:20 -0500

    Create covidVaxWatch.py

---
+++ covidVaxWatch.py
@@ -0,0 +1,44 @@
+import requests 
+from twilio.rest import Client
+import time
+import datetime
+# import winsound
+
+# https://www.fullstackpython.com/blog/send-sms-text-messages-python.html
+twilioAccountId = ""
+twilioAuthToken = ""
+twilioClient = Client(twilioAccountId, twilioAuthToken)
+backendApiCall = "https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers"
+myPhoneNumber = ""
+twilioPhoneNumber = ""
+providerNameList = [
+    "SUNY Albany ",
+    "SUNY Albany"
+]
+delay = 60
+PROVIDER_LIST = "providerList"
+PROVIDER_NAME = "providerName"
+AVAILABLE_APPOINTMENTS = "availableAppointments"
+APPOINTMENTS_AVAILABLE_CODE = "AA"
+NO_APPOINTMENTS_CURRENTLY_CODE = "NAC"
+
+while (True):
+    response = requests.get(backendApiCall) 
+    providerList = response.json()[PROVIDER_LIST]
+    for i in range(len(providerList)):
+        providerName = providerList[i][PROVIDER_NAME]
+        if (providerName in providerNameList):
+            availableAppointments = providerList[i][AVAILABLE_APPOINTMENTS]            
+            if (availableAppointments == APPOINTMENTS_AVAILABLE_CODE):
+                print("Appointments Available at '" + providerName + "'!")
+                twilioClient.messages.create(
+                    to=myPhoneNumber,
+                    from_=twilioPhoneNumber,
+                    body="Appointments Available at '" + providerName + "'!  Go to https://am-i-eligible.covid19vaccine.health.ny.gov/ to reserve a spot now!")
+                # while(True):
+                #     winsound.PlaySound('ding.wav', winsound.SND_FILENAME)
+                exit()
+            if (availableAppointments == NO_APPOINTMENTS_CURRENTLY_CODE):
+                datetime_object = datetime.datetime.now()
+                print("None Available Currently for '" + providerName + "' as of", datetime_object)
+    time.sleep(delay)
